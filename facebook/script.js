f45f62e883b0b31a83d66de0bc94f133d4d7b764
Aditya Narayan Jaiswal <85832994+AdityaNarayan05@users.noreply.github.com>
2022-06-02 12:51:35 +0530

    Add files via upload

--- facebook/script.js
+++ facebook/script.js
@@ -2,7 +2,16 @@ var database=[
     {
         username:"Aditya",
         password:"9919276694"
+    },
+    {
+        username:"sally",
+        password:"123"
+    },
+    {
+        username:"Mr.X",
+        password:"xyz"
     }
+
 ];
 var newsFeed=[
     {
@@ -12,17 +21,31 @@ var newsFeed=[
     {
         username:"Aj",
         timeline:"javascript is little tricky to understand"
+    },
+    {
+        username:"Sally",
+        timeline:"javascript is preety cool!"
     }
 ];
 
-var usernamePrompt=prompt("What's your username?");
-var passwordPrompt=prompt("What's your password");
+function isUserValid(username,password){
+    for(var i=0;i<database.length;i++){
+        if(username===database[i].username&&password===database[i].password){
+            return true;
+        }
+    }
+    return false;
+}
 
 function signIn(user,pass){
-    if(user===database[0].username&&pass===database[0].password){
-        console.log(newsFeed);
-    } else{
-        alert("Sorry,wrong username and password!");
-    }
+    console.log(isUserValid(user,pass));
+    //if(user===database[0].username&&pass===database[0].password){
+    //    console.log(newsFeed);
+    //} else{
+    //    alert("Sorry,wrong username and password!");
+    //}
 }
+
+var usernamePrompt=prompt("What's your username?");
+var passwordPrompt=prompt("What's your password");
 signIn(usernamePrompt,passwordPrompt);
